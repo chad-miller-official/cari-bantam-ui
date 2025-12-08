dc6b44cc7e7dd4b7fe5dae80058f4398a5d3125e
Chad Miller <chad.miller.3333@gmail.com>
2025-12-07 23:26:22 -0500

    Improved modal

--- src/aesthetic/aesthetic-page-2.ts
+++ src/aesthetic/aesthetic-page-2.ts
@@ -1,16 +1,93 @@
 import {AestheticBlock} from "./components/aesthetic-block";
 import {ArenaApiResponse, BlockClass, GalleryContent} from "./types";
 import InfiniteScroll from "infinite-scroll";
+import CariSpinner from "../components/spinner";
+import {FullscreenModal} from "../components/modal";
 
 declare const mediaSourceUrl: string
 
 const MAX_PAGE_SIZE = 20
 
 let totalPages = 1
-let loadedLast = true
+let loadedLast = false
 
-function buildBlock(block: GalleryContent): JQuery<HTMLElement> {
-  const blockElement = $('<div>').addClass('aesthetic-gallery-block')
+let blocks: GalleryContent[] = []
+
+function openBlock(block: GalleryContent, idx: number, infScroll: InfiniteScroll) {
+  let media = $('<p>').text('This media type is not supported.')
+
+  switch (block.class) {
+    case BlockClass.Link:
+    case BlockClass.Image:
+      media = $('<a>')
+      .attr('href', block.source?.url || block.image.original.url)
+      .attr('target', '_blank')
+      .append(
+          $('<img>')
+          .attr('src', block.image.display.url)
+          .attr('alt', block.description)
+      )
+
+      break
+    case BlockClass.Attachment:
+      const contentType = block.attachment.content_type
+
+      if (contentType.split('/')[0] === 'video') {
+        const source = $('<source>')
+        .attr('src', block.attachment.url)
+        .text('Your browser does not support video playback.')
+        media = $('<video autoplay controls muted>')
+        .append(source)
+      } else if (contentType === 'application/pdf') {
+        media = $('<object>')
+        .attr('data', block.attachment.url)
+        .attr('type', contentType)
+        .addClass('attachment-media')
+      }
+
+      break
+    case BlockClass.Media:
+      media = $(block.embed.html)
+      break
+    case BlockClass.Text:
+      media = $('<div>')
+      .addClass('text-media')
+      .html(block.content_html)
+
+      break
+  }
+
+  $('#aestheticGallerySelection > .media').empty().append(media)
+  $('#aestheticGallerySelection > .sidebar > .title').text(block.title || '(no title)')
+  $('#aestheticGallerySelection > .sidebar > .description').html(block.description_html || '(no description)')
+
+  window.onkeyup = (event) => {
+    if (event.key === 'ArrowLeft' && idx > 0) {
+      openBlock(blocks[idx - 1], idx - 1, infScroll)
+    } else if (event.key === 'ArrowRight') {
+      if (idx < blocks.length - 1) {
+        openBlock(blocks[idx + 1], idx + 1, infScroll)
+      } else {
+        $('#aestheticGallerySelection > .media').empty().append($(new CariSpinner()))
+
+        loadNextPage($('#aestheticGallery'), infScroll, () => {
+          openBlock(blocks[idx + 1], idx + 1, infScroll)
+        })
+      }
+    }
+  }
+
+  $('#aestheticGallery').css('overflow', 'hidden')
+
+  const modal = $('fullscreen-modal');
+  (modal.get(0) as FullscreenModal).showModal()
+}
+
+function buildBlock(block: GalleryContent, idx: number, infScroll: InfiniteScroll): JQuery<HTMLElement> {
+  const blockElement = $('<div>')
+  .addClass('aesthetic-gallery-block')
+  .on('click', () => openBlock(block, idx, infScroll))
+
   let content: JQuery<HTMLElement>
 
   if (
@@ -19,16 +96,37 @@ function buildBlock(block: GalleryContent): JQuery<HTMLElement> {
       block.class === BlockClass.Media ||
       (block.class === BlockClass.Attachment && block.image)
   ) {
-    content = $('<img>')
+    content = $('<img>').addClass('image-preview')
     .attr('src', block.image.square.url)
     .attr('alt', block.title)
   } else {
     content = block.class === BlockClass.Text
-        ? $('<p>').text(block.content.length > 100 ? block.content.substring(0, 97) + '...' : block.content)
+        ? $('<p>').addClass('text-preview').text(block.content)
         : $('<h3>').text('No Preview')
   }
 
   return blockElement.append(content)
+}
+
+function loadNextPage(container: JQuery<HTMLElement>, infScroll: InfiniteScroll, loadedCallback?: () => void) {
+  let cariSpinner = container.children('cari-spinner')
+
+  if (!cariSpinner.length) {
+    cariSpinner = $(new CariSpinner())
+  }
+
+  container.append(cariSpinner)
+  const loadNextPage = infScroll.loadNextPage()
+
+  if (loadNextPage) {
+    loadNextPage.then(() => {
+      cariSpinner.remove()
+
+      if (loadedCallback) {
+        loadedCallback()
+      }
+    })
+  }
 }
 
 $(() => {
@@ -39,11 +137,6 @@ $(() => {
       page: 1,
       per: MAX_PAGE_SIZE,
     }
-
-    $.get(mediaSourceUrl, data, (res: ArenaApiResponse) => {
-      totalPages = Math.ceil(res.length / MAX_PAGE_SIZE)
-      aestheticGallery.append(...res.contents.map(buildBlock))
-    })
 
     const infScroll = new InfiniteScroll(aestheticGallery.get(0), {
       path: () => {
@@ -58,30 +151,50 @@ $(() => {
       scrollThreshold: false,
     })
 
-    infScroll.on('load', (res: ArenaApiResponse) => aestheticGallery.append(...res.contents.map(buildBlock)))
+    $.get(mediaSourceUrl, data, (res: ArenaApiResponse) => {
+      totalPages = Math.ceil(res.length / MAX_PAGE_SIZE)
+      aestheticGallery.append(...res.contents.map((block, idx) => buildBlock(block, idx, infScroll)))
+      blocks.push(...res.contents)
+    })
+
+    infScroll.on('load', (res: ArenaApiResponse) => {
+      aestheticGallery.append(...res.contents.map((block, idx) => buildBlock(block, idx, infScroll)))
+      blocks.push(...res.contents)
+    })
+
     infScroll.on('last', () => loadedLast = true)
 
     aestheticGallery.on('scroll', function (event) {
       const trueWidth = this.scrollWidth - $(this).parent().width()
+      let maskImage = 'initial'
 
       if (event.target.scrollLeft === 0) {
-        $('#aestheticGalleryContainer').css('mask-image', 'linear-gradient(to right, black 0%, black 90%, transparent 99%)')
+        maskImage = 'linear-gradient(to right, black 0%, black 90%, transparent 99%)'
       } else if (event.target.scrollLeft > 0 && (!loadedLast || event.target.scrollLeft < trueWidth)) {
-        $('#aestheticGalleryContainer').css('mask-image', 'linear-gradient(to right, transparent 1%, black 10%, black 90%, transparent 99%)')
+        maskImage = 'linear-gradient(to right, transparent 1%, black 10%, black 90%, transparent 99%)'
       } else if (event.target.scrollLeft === trueWidth) {
-        $('#aestheticGalleryContainer').css('mask-image', 'linear-gradient(to right, transparent 1%, black 10%, black 100%)')
+        maskImage = 'linear-gradient(to right, transparent 1%, black 10%, black 100%)'
       }
 
-      if (event.target.scrollLeft >= 0.9 * trueWidth) {
-        infScroll.loadNextPage()
+      $('#aestheticGalleryContainer').css('mask-image', maskImage)
+
+      if (!loadedLast && event.target.scrollLeft >= 0.9 * trueWidth) {
+        loadNextPage($(this), infScroll)
       }
     })
 
     aestheticGallery.on('wheel', function (event) {
       event.preventDefault()
-      this.scrollLeft += (event.originalEvent as WheelEvent).deltaY
+      this.scrollBy({left: (event.originalEvent as WheelEvent).deltaY})
+    })
+
+    const originalWindowOnKeyUp = window.onkeyup
+
+    $('fullscreen-modal').on('close', () => {
+      window.onkeyup = originalWindowOnKeyUp
+      aestheticGallery.css('overflow', '')
     })
   }
 })
 
-export {AestheticBlock}
+export {AestheticBlock, FullscreenModal}
--- src/components/modal.ts
+++ src/components/modal.ts
@@ -1,4 +1,4 @@
-import {css, html, LitElement} from 'lit'
+import {css, html, LitElement, PropertyValueMap, PropertyValues} from 'lit'
 import {customElement, query, state} from 'lit/decorators.js'
 
 @customElement('cari-modal')
@@ -25,7 +25,7 @@ export default class CariModal extends LitElement {
       top: 50%;
       transform: translate(-50%, -50%);
     }
-    
+
     button {
       font-size: large;
       margin-bottom: 4px;
@@ -78,6 +78,81 @@ export default class CariModal extends LitElement {
           </button>
           <slot></slot>
         </div>
+      </dialog>`
+  }
+}
+
+@customElement('fullscreen-modal')
+export class FullscreenModal extends LitElement {
+  static styles = css`
+    dialog {
+      background-color: rgba(255, 255, 255, 90%);
+      border: none;
+      height: 100%;
+      left: 0;
+      margin: 0;
+      max-height: unset;
+      max-width: unset;
+      padding: 0;
+      position: fixed;
+      top: 0;
+      width: 100%;
+      z-index: 10000;
+    }
+
+    .modal-actions {
+      position: fixed;
+      top: 0;
+    }
+
+    .modal-actions > button {
+      background-color: initial;
+      border: none;
+      color: #7f7f7f;
+      cursor: pointer;
+      font-size: 36px;
+      padding: 8px;
+
+      &:hover {
+        background-color: initial;
+        color: #414141;
+      }
+    }`
+
+  @query('#modal')
+  modal: HTMLDialogElement
+
+  firstUpdated(changedProperties: PropertyValues) {
+    super.firstUpdated(changedProperties);
+
+    this.modal.addEventListener('close', (event) => {
+      document.body.style.overflowY = 'unset'
+      this.dispatchEvent(new Event('close'))
+    })
+  }
+
+  showModal() {
+    document.body.style.overflow = 'hidden'
+    this.modal.showModal()
+  }
+
+  render() {
+    return html`
+      <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/fontawesome.min.css"
+            integrity="sha512-J2Gce+WmOttffHOrVKLTlzxIalPXUMDbSfn5ADqp8Vj9EngnjNHr+jjiL3ZB8muEzo+K51gU10X+0eGqGNL7QA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"/>
+      <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/solid.min.css"
+            integrity="sha512-SRwBRtvmT+LZLST1GBrRec8GROqtjKU1NK23RKSpdL6IRzn2xPeOfBYSeIlc/9iXYagKB5IX6MyKZF1nq4a1fw=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"/>
+      <dialog id="modal">
+        <slot></slot>
+        <form method="dialog" class="modal-actions">
+          <button>
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </form>
       </dialog>`
   }
 }
