372e3d1c5a801f579018c970328305f2161de28b
Chad Miller <chad.miller.3333@gmail.com>
2026-06-13 00:36:43 -0400

    Restored functionality

--- src/cms/job/job-control-panel.ts
+++ src/cms/job/job-control-panel.ts
@@ -8,57 +8,27 @@ import {waitFor} from "../../util";
 declare const _csrf: Csrf
 declare const jobEndpoint: string
 
-declare let lastJobExecution: number
-declare let lastJobExecutionLog: number
-declare let lastJobExecutionStatus: number
+declare const lastJobExecution: number
+declare const lastJobExecutionLog: number
+declare const lastJobExecutionStatus: number
 
-let selectedJobExecution = lastJobExecution
+let _lastJobExecution: number
+let _lastJobExecutionLog: number
+let _lastJobExecutionStatus: number
+
+let selectedJobExecution: number
 let showHiddenLogs = false
+let stompClient: Client
 
-let stompClient = new Client({
-  brokerURL: '/cari-websocket',
-  onConnect: () => {
-    stompClient.subscribe('/topic/job-data', (data: IMessage) => {
-      const response = JSON.parse(data.body) as JobDataRequestResponse
-
-      if (response.last) {
-        lastJobExecutionStatus = response.jobExecutionStatus
-        updateJobExecutionStatus(response.status)
-
-        $('#outputFileUrl').prop('href', response.outputFileUrl)
-      }
-
-      ($('#jobProgressBar').get(0) as CariProgressBar).percentComplete = response.percentComplete
-
-      if (response.logs.length > 0) {
-        lastJobExecutionLog = response.logs[0].jobExecutionLog
-      }
-
-      if (response.jobExecution !== selectedJobExecution) {
-        return
-      }
-
-      appendLogs(response.logs, false)
-    })
-
-    pullLogs().then(() => stompClient.deactivate())
-  },
-  onWebSocketError: error => {
-    console.error('WebSocket error', error)
-  },
-  onStompError: frame => {
-    console.error(`Broker reported error: ${frame.headers['message']}`)
-    console.error(`Additional details: ${frame.body}`)
-  }
-})
+/* LOG FUNCTIONS */
 
 async function pullLogs() {
-  while (lastJobExecutionStatus === 1) {
+  while (_lastJobExecutionStatus === 1) {
     stompClient.publish({
       destination: '/app/pull-job-data',
       body: JSON.stringify({
-        jobExecution: lastJobExecution,
-        lastJobExecutionLog: lastJobExecutionLog,
+        jobExecution: _lastJobExecution,
+        lastJobExecutionLog: _lastJobExecutionLog,
       })
     })
 
@@ -66,8 +36,46 @@ async function pullLogs() {
   }
 }
 
+function appendLogs(logs: JobLog[], reset: boolean) {
+  const newLogs = logs.map(log => $('<code>')
+    .text(log.formattedMessage)
+    .prop('hidden', !showHiddenLogs && log.logLevel === 1)
+    .data('logLevel', log.logLevel)
+    .attr('data-log-level', log.logLevel)
+  )
+
+  const logViewer = $('#logs')
+
+  if (reset) {
+    logViewer.empty()
+  }
+
+  logViewer.prepend(newLogs)
+}
+
+function toggleDebugLogs() {
+  showHiddenLogs = !showHiddenLogs
+
+  $('#logs').children().each((_, child) => {
+    const $child = $(child)
+    const logLevel = parseInt($child.data('logLevel'))
+
+    if (showHiddenLogs) {
+      $child.removeAttr('hidden')
+    } else if (logLevel === 1) {
+      $child.attr('hidden', 'hidden')
+    }
+  })
+}
+
+/* JOB FUNCTIONS */
+
+function getPreviousJobs() {
+  return $('#jobsHistory > ol').children() as JQuery<HTMLLIElement>
+}
+
 function invokeJob() {
-  if (lastJobExecutionStatus === 1) {
+  if (_lastJobExecutionStatus === 1) {
     alert('Job is already running.')
     return
   }
@@ -79,82 +87,65 @@ function invokeJob() {
   }
 
   axios.post<JobResponse>(jobEndpoint, {}, axiosConfig)
-  .then((res: AxiosResponse<JobResponse>) => {
-    const jobResponse = res.data
-    const error = jobResponse.error
+    .then((res: AxiosResponse<JobResponse>) => {
+      const jobResponse = res.data
+      const error = jobResponse.error
 
-    if (error) {
-      alert(error.message)
-    } else {
-      const jobExecution = jobResponse.jobExecution;
+      if (error) {
+        alert(error.message)
+      } else {
+        const jobExecution = jobResponse.jobExecution
 
-      ($('#jobProgressBar').get(0) as CariProgressBar).percentComplete = 0
+        _lastJobExecution = jobExecution
+        _lastJobExecutionLog = 0
+        _lastJobExecutionStatus = 1
 
-      lastJobExecution = jobExecution
-      lastJobExecutionLog = 0
-      lastJobExecutionStatus = 1
-      selectedJobExecution = jobExecution
+        $('#runJob').attr('disabled', 'disabled')
 
-      appendJobExecution(jobExecution, jobResponse.started)
-      appendLogs([], true)
+        const progressBar = new CariProgressBar()
+        progressBar.percentComplete = 0
 
-      $('#outputFileUrl').prop('href', '#').prop('disabled', 'disabled')
+        $('#jobProgressBar').append(progressBar)
 
-      stompClient.activate()
-    }
-  })
-  .catch((err: AxiosError) => {
-    alert(`Job failed to start. Reason: ${err.message}`)
-  })
-}
+        selectedJobExecution = jobExecution
 
-function getPreviousJobs() {
-  return $('#jobsHistory > ol').children() as JQuery<HTMLLIElement>
+        appendJobExecution(jobExecution, jobResponse.started)
+        appendLogs([], true)
+
+        $('#outputFileUrl').prop('href', '#').prop('disabled', 'disabled')
+
+        stompClient.activate()
+      }
+    })
+    .catch((err: AxiosError) => {
+      alert(`Job failed to start. Reason: ${err.message}`)
+    })
 }
 
 function clearSelectedJob() {
   getPreviousJobs().each((_, child) => {
-    $(child).removeProp('selected')
+    $(child).removeAttr('selected')
   })
 }
 
-function appendLogs(logs: JobLog[], reset: boolean) {
-  const newLogs = logs.map(log => {
-    const logItem = document.createElement('code')
-
-    logItem.innerText = log.formattedMessage
-    logItem.hidden = !showHiddenLogs && log.logLevel === 1
-    logItem.dataset.logLevel = log.logLevel.toString()
-    logItem.slot = 'logs'
-
-    return logItem
-  })
-
-  const logViewer = $('#logs')
-
-  if (reset) {
-    logViewer.children().replaceWith(newLogs)
-  } else {
-    logViewer.prepend(newLogs)
-  }
-}
+/* JOB EXECUTION FUNCTIONS */
 
 function displayJobExecution(jobHistoryEntry: JQuery<HTMLLIElement>) {
-  if (jobHistoryEntry.prop('selected')) {
+  if (jobHistoryEntry.attr('selected')) {
     return
   }
 
   clearSelectedJob()
-  jobHistoryEntry.prop('selected', true)
+  jobHistoryEntry.attr('selected', 'selected')
 
   const jobExecution = jobHistoryEntry.data('jobExecution')
 
   axios.get<JobHistoryResponse>('/api/jobs/get-job-execution-data', {params: {jobExecution}})
-  .then((res: AxiosResponse<JobHistoryResponse>) => {
-    selectedJobExecution = jobExecution
-    $('#outputFileUrl').prop('href', res.data.outputFileUrl).removeProp('disabled')
-    appendLogs(res.data.logs, true)
-  })
+    .then((res: AxiosResponse<JobHistoryResponse>) => {
+      selectedJobExecution = jobExecution
+      $('#outputFileUrl').prop('href', res.data.outputFileUrl).removeProp('disabled')
+      appendLogs(res.data.logs, true)
+    })
 }
 
 function appendJobExecution(jobExecution: number, started: string) {
@@ -164,50 +155,78 @@ function appendJobExecution(jobExecution: number, started: string) {
   const jobHistoryEntry = $(document.importNode(jobHistoryEntryTemplate.content, true).firstElementChild as HTMLLIElement)
 
   jobHistoryEntry.prop('selected', true)
-  jobHistoryEntry.data('jobExecution', jobExecution)
-  jobHistoryEntry.children('.job-execution').text(jobExecution)
-  jobHistoryEntry.children('.job-started').text(new Date(started).toUTCString().split(' ').slice(1, 5).join(' '))
-  jobHistoryEntry.on('click', () => displayJobExecution(jobHistoryEntry))
+    .data('jobExecution', jobExecution)
+    .on('click', () => displayJobExecution(jobHistoryEntry))
 
-  $('#jobsHistory').prepend(jobHistoryEntry)
+  jobHistoryEntry.find('.job-execution')
+    .text(jobExecution)
+
+  jobHistoryEntry.find('.job-started')
+    .text(new Date(started).toUTCString().split(' ').slice(1, 5).join(' '))
+
+  $('#jobsHistory > ol').prepend(jobHistoryEntry)
 }
 
-function updateJobExecutionStatus(status: string) {
-  const previousJobs = getPreviousJobs()
-  let jobStatus = previousJobs.find('[slot=jobStatus]')
-
-  if (jobStatus == null) {
-    jobStatus = $('<span>')
-    jobStatus.prop('slot', 'jobStatus')
-    previousJobs.append(jobStatus)
-  }
-
-  jobStatus.text(status)
-}
-
-function toggleDebugLogs() {
-  showHiddenLogs = !showHiddenLogs
-
-  $('#logs').children().each((_, child) => {
-    const logLevel = parseInt(child.dataset.logLevel)
-
-    if (showHiddenLogs) {
-      child.removeAttribute('hidden')
-    } else if (logLevel === 1) {
-      child.setAttribute('hidden', '')
-    }
-  })
-}
+/* END FUNCTIONS */
 
 $(() => {
+  _lastJobExecution = lastJobExecution
+  _lastJobExecutionLog = lastJobExecutionLog
+  _lastJobExecutionStatus = lastJobExecutionStatus
+
+  selectedJobExecution = lastJobExecution
+
   $('#runJob').on('click', invokeJob)
   $('#showDebugLogs').on('click', toggleDebugLogs)
+
+  $('#jobsHistory > ol').children().each((_, child) => {
+    child.onclick = () => displayJobExecution($(child as HTMLLIElement))
+  })
+
+  stompClient = new Client({
+    brokerURL: '/cari-websocket',
+    onConnect: () => {
+      stompClient.subscribe('/topic/job-data', (data: IMessage) => {
+        const response = JSON.parse(data.body) as JobDataRequestResponse
+        const progressBar = $('#jobProgressBar')
+
+        if (response.last) {
+          _lastJobExecutionStatus = response.jobExecutionStatus
+
+          if (response.jobExecutionStatus !== 5) {
+            $('#runJob').removeAttr('disabled')
+          }
+
+          progressBar.empty()
+          getPreviousJobs().find('.job-status').first().text(response.status)
+          $('#outputFileUrl').prop('href', response.outputFileUrl)
+        } else {
+          (progressBar.children('cari-progress-bar').get(0) as CariProgressBar).percentComplete = response.percentComplete
+        }
+
+        if (response.logs.length > 0) {
+          _lastJobExecutionLog = response.logs[0].jobExecutionLog
+        }
+
+        if (response.jobExecution !== selectedJobExecution) {
+          return
+        }
+
+        appendLogs(response.logs, false)
+      })
+
+      pullLogs().then(() => stompClient.deactivate())
+    },
+    onWebSocketError: error => {
+      console.error('WebSocket error', error)
+    },
+    onStompError: frame => {
+      console.error(`Broker reported error: ${frame.headers['message']}`)
+      console.error(`Additional details: ${frame.body}`)
+    }
+  })
 
   if (lastJobExecutionStatus === 1) {
     stompClient.activate()
   }
-
-  $('#jobHistory > ol').children().each((_, child) => {
-    child.onclick = () => displayJobExecution($(child as HTMLLIElement))
-  })
 })
